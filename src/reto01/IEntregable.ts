6b73b78da66420b10982a304d97471e060fb027f
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-02 20:38:28 +0200

    Cambios varios en cada ejercicio menos el reto 3

---
+++ src/reto01/IEntregable.ts
@@ -0,0 +1,6 @@
+interface IEntregable{
+    entregar():void; //Cambia el boolean 
+    devolver():void; //Cambia el boolean 
+    isEntregado():boolean; //devuelve el boolean 
+    compareTo(a:Object):number;
+}
